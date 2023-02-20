0022edecb1e1b1ccfe5f7d860044df3649e738bc
MohammadNasir520 <nasirahsan520@gmail.com>
2023-02-20 21:41:01 +0600

    correction

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../Pages/Home/Home";
 import Main from "../layout/Main";
-import AddUser from "../Pages/AddUser/AddUser";
+
 import EditUser from "../Pages/EditUser/EditUser";
 
 export const router = createBrowserRouter([
@@ -13,10 +13,7 @@ export const router = createBrowserRouter([
                 path: "/",
                 element: <Home></Home>
             },
-            {
-                path: "/addUser",
-                element: <AddUser />
-            },
+
             {
                 path: "/editUser/:id",
                 loader: ({ params }) => fetch(`https://doyo-pet-fire-store.vercel.app/users/${params.id}`),
